5ee622cdab086714efb1c8ff493b26e3d295d64b
ImminentFate <fatefulproductions@hotmail.com>
2017-11-09 12:43:04 +1000

    Made app single-instance only

--- WindowsApp1/My Project/Application.Designer.vb
+++ WindowsApp1/My Project/Application.Designer.vb
@@ -24,7 +24,7 @@ Namespace My
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Public Sub New()
             MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
-            Me.IsSingleInstance = false
+            Me.IsSingleInstance = true
             Me.EnableVisualStyles = true
             Me.SaveMySettingsOnExit = true
             Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
