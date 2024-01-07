faf84ff41dba873997f2b7838a3205f4284617dd
IridiumIO <iridium.io@outlook.com>
2024-01-07 12:21:39 +1000

    Version bump to 3.0.2

--- CompactGUI/Models/UpdateHandler.vb
+++ CompactGUI/Models/UpdateHandler.vb
@@ -2,7 +2,7 @@
 Imports System.Text.Json
 Public Class UpdateHandler
 
-    Public Shared CurrentVersion As New SemVersion(3, 0, 1)
+    Public Shared CurrentVersion As New SemVersion(3, 0, 2)
     Public Shared NewVersion As SemVersion
     Shared UpdateURL As String = "https://raw.githubusercontent.com/IridiumIO/CompactGUI/database/version.json"
 
