f13ab30e824e305152d6e1e66f594a1e1afb3f35
IridiumIO <iridium.io@outlook.com>
2024-07-01 20:50:23 +1000

    Refactor Core.Analyser to make code clearer

--- CompactGUI.Core/Analyser.vb
+++ CompactGUI.Core/Analyser.vb
@@ -5,60 +5,76 @@ Imports System.Threading
 
 Public Class Analyser
 
-    Public Sub New(folder As String)
-        FolderName = folder
-    End Sub
-
     Public Property FolderName As String
     Public Property UncompressedBytes As Long
     Public Property CompressedBytes As Long
     Public Property ContainsCompressedFiles As Boolean
     Public Property FileCompressionDetailsList As List(Of AnalysedFileDetails)
-    Private _testField As Integer = 0
+
+    Public Sub New(folder As String)
+        FolderName = folder
+    End Sub
+
 
     Public Async Function AnalyseFolder(cancellationToken As CancellationToken) As Task(Of Boolean)
-
         Dim allFiles = Await Task.Run(Function() Directory.EnumerateFiles(FolderName, "*", New EnumerationOptions() With {.RecurseSubdirectories = True, .IgnoreInaccessible = True}).AsShortPathNames, cancellationToken).ConfigureAwait(False)
-        Dim compressedFilesCount As Integer
         Dim fileDetails As New Concurrent.ConcurrentBag(Of AnalysedFileDetails)
+        Dim compressedFilesCount As Integer = 0
+
+        ' Use local variables to reduce contention
+        Dim localCompressedBytes As Long = 0
+        Dim localUncompressedBytes As Long = 0
 
         Try
-            Dim res = Await Task.Run(Function() Parallel.ForEach(allFiles, New ParallelOptions With {.CancellationToken = cancellationToken}, Sub(file) AnalyseFile(file, compressedFilesCount, fileDetails)))
+            Parallel.ForEach(allFiles, New ParallelOptions With {.CancellationToken = cancellationToken},
+                         Sub(file)
+                             Dim details = AnalyseFile(file)
+                             If details IsNot Nothing Then
+                                 fileDetails.Add(details)
+                                 If details.CompressionMode <> CompressionAlgorithm.NO_COMPRESSION Then
+                                     Interlocked.Increment(compressedFilesCount)
+                                 End If
+                                 Interlocked.Add(localCompressedBytes, details.CompressedSize)
+                                 Interlocked.Add(localUncompressedBytes, details.UncompressedSize)
+                             End If
+                         End Sub)
+
+            ' Update the shared state after the parallel loop to minimize contention
+            CompressedBytes = localCompressedBytes
+            UncompressedBytes = localUncompressedBytes
         Catch ex As OperationCanceledException
             Debug.WriteLine(ex.Message)
             Return Nothing
         End Try
 
-        ContainsCompressedFiles = compressedFilesCount <> 0
+        Debug.WriteLine(CompressedBytes)
+        Debug.WriteLine(UncompressedBytes)
+
+
+        ContainsCompressedFiles = compressedFilesCount > 0
         FileCompressionDetailsList = fileDetails.ToList
         Return ContainsCompressedFiles
-
     End Function
 
 
-    Private Sub AnalyseFile(file As String, ByRef compressedFilesCount As Integer, ByRef fileDetails As Concurrent.ConcurrentBag(Of AnalysedFileDetails))
-
+    Private Function AnalyseFile(file As String) As AnalysedFileDetails
         Try
             Dim fInfo As New FileInfo(file)
             Dim unCompSize = fInfo.Length
             Dim compSize = GetFileSizeOnDisk(file)
             If compSize < 0 Then
-                'GetFileSizeOnDisk failed, fall back to unCompSize
-                compSize = unCompSize
+                compSize = unCompSize ' GetFileSizeOnDisk failed, fall back to unCompSize
             End If
             Dim cLevel As CompressionAlgorithm = If(compSize = unCompSize, CompressionAlgorithm.NO_COMPRESSION, DetectCompression(fInfo))
 
-            'Sets the backing private fields directly because Interlocked doesn't play nice with properties!
-            Interlocked.Add(_CompressedBytes, compSize)
-            Interlocked.Add(_UncompressedBytes, unCompSize)
-            Interlocked.Add(_testField, 1)
-            fileDetails.Add(New AnalysedFileDetails With {.FileName = file, .CompressedSize = compSize, .UncompressedSize = unCompSize, .CompressionMode = cLevel})
-            If cLevel <> CompressionAlgorithm.NO_COMPRESSION Then Interlocked.Increment(compressedFilesCount)
+            Return New AnalysedFileDetails With {.FileName = file, .CompressedSize = compSize, .UncompressedSize = unCompSize, .CompressionMode = cLevel}
         Catch ex As IOException
             Debug.WriteLine($"Error analysing file {file}: {ex.Message}")
+            Return Nothing
         End Try
+    End Function
 
-    End Sub
+
     Public Async Function GetPoorlyCompressedExtensions() As Task(Of List(Of ExtensionResult))
         Dim extClassResults As New List(Of ExtensionResult)
         Await Task.Run(
@@ -70,22 +86,22 @@ Public Class Analyser
                                    If fl.UncompressedSize = 0 Then Return
 
                                    extRes.AddOrUpdate(xt,
-                                    Function(addKey) ' Add value factory
-                                        Return New ExtensionResult With {
+                                        Function(addKey) ' Add value factory
+                                            Return New ExtensionResult With {
                                             .extension = xt,
                                             .totalFiles = 1,
                                             .uncompressedBytes = fl.UncompressedSize,
                                             .compressedBytes = fl.CompressedSize
                                         }
-                                    End Function,
-                                    Function(updateKey, oldValue) ' Update value factory
-                                        Return New ExtensionResult With {
+                                        End Function,
+                                        Function(updateKey, oldValue) ' Update value factory
+                                            Return New ExtensionResult With {
                                             .extension = xt,
                                             .totalFiles = oldValue.totalFiles + 1,
                                             .uncompressedBytes = oldValue.uncompressedBytes + fl.UncompressedSize,
                                             .compressedBytes = oldValue.compressedBytes + fl.CompressedSize
                                         }
-                                    End Function)
+                                        End Function)
                                End Sub)
 
             ' Filter and convert to list after aggregation
@@ -94,6 +110,7 @@ Public Class Analyser
 
         Return extClassResults
     End Function
+
 
     Private Function DetectCompression(fInfo As FileInfo) As CompressionAlgorithm
 
@@ -109,45 +126,6 @@ Public Class Analyser
 
     End Function
 
-
-
-    'Public Function HasDirectoryWritePermission() As Boolean
-
-    '    Try
-    '        Dim ACRules = New DirectoryInfo(FolderName).GetAccessControl().GetAccessRules(True, True, GetType(Security.Principal.SecurityIdentifier))
-
-    '        Dim identity = Security.Principal.WindowsIdentity.GetCurrent
-    '        Dim principal = New Security.Principal.WindowsPrincipal(identity)
-    '        Dim writeDenied = False
-
-    '        For Each FSRule As FileSystemAccessRule In ACRules
-    '            If (FSRule.FileSystemRights And FileSystemRights.Write) = 0 Then Continue For
-
-
-    '            ' Use Translate to safely convert to NTAccount
-    '            Dim ntAccount As Security.Principal.NTAccount = Nothing
-    '            Try
-    '                ntAccount = DirectCast(FSRule.IdentityReference.Translate(GetType(Security.Principal.NTAccount)), System.Security.Principal.NTAccount)
-    '            Catch ex As Exception
-    '                Continue For
-    '            End Try
-
-    '            If ntAccount Is Nothing OrElse Not principal.IsInRole(ntAccount.Value) Then Continue For
-
-    '            If FSRule.AccessControlType = AccessControlType.Deny Then
-    '                writeDenied = True
-    '                Exit For
-    '            End If
-
-    '        Next
-
-    '        Return Not writeDenied
-    '    Catch ex As UnauthorizedAccessException
-    '        
-    '        Return False
-    '    End Try
-
-    'End Function
 
     Public Function HasDirectoryWritePermission() As Boolean
         Try
