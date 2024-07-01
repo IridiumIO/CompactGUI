5e9b9f87bb56f28fb39a4a9be03223c52259ec06
IridiumIO <iridium.io@outlook.com>
2024-07-01 20:49:56 +1000

    Speed up GetPoorlyCompressedExtensions by up to ~10x

--- CompactGUI.Core/Analyser.vb
+++ CompactGUI.Core/Analyser.vb
@@ -59,29 +59,40 @@ Public Class Analyser
         End Try
 
     End Sub
-
-
     Public Async Function GetPoorlyCompressedExtensions() As Task(Of List(Of ExtensionResult))
-        Dim extClassResults As List(Of ExtensionResult) = Await Task.Run(
-            Function()
-                Dim extRes As New List(Of ExtensionResult)
-                For Each fl In FileCompressionDetailsList
-                    Dim fInfo As New IO.FileInfo(fl.FileName)
-                    Dim xt = fInfo.Extension
-                    If fl.UncompressedSize = 0 Then Continue For
-                    Dim obj = extRes.FirstOrDefault(Function(x) x.extension = xt, Nothing)
-                    If obj Is Nothing Then
-                        extRes.Add(New ExtensionResult With {.extension = xt, .totalFiles = 1, .uncompressedBytes = fl.UncompressedSize, .compressedBytes = fl.CompressedSize})
-                        Continue For
-                    End If
-                    obj.uncompressedBytes += fl.UncompressedSize
-                    obj.compressedBytes += fl.CompressedSize
-                    obj.totalFiles += 1
-                Next
-                Return extRes
-            End Function)
+        Dim extClassResults As New List(Of ExtensionResult)
+        Await Task.Run(
+        Sub()
+            Dim extRes As New Concurrent.ConcurrentDictionary(Of String, ExtensionResult)
+            Parallel.ForEach(FileCompressionDetailsList,
+                               Sub(fl)
+                                   Dim xt = New IO.FileInfo(fl.FileName).Extension
+                                   If fl.UncompressedSize = 0 Then Return
 
-        Return extClassResults.Where(Function(f) f.cRatio > 0.95).ToList()
+                                   extRes.AddOrUpdate(xt,
+                                    Function(addKey) ' Add value factory
+                                        Return New ExtensionResult With {
+                                            .extension = xt,
+                                            .totalFiles = 1,
+                                            .uncompressedBytes = fl.UncompressedSize,
+                                            .compressedBytes = fl.CompressedSize
+                                        }
+                                    End Function,
+                                    Function(updateKey, oldValue) ' Update value factory
+                                        Return New ExtensionResult With {
+                                            .extension = xt,
+                                            .totalFiles = oldValue.totalFiles + 1,
+                                            .uncompressedBytes = oldValue.uncompressedBytes + fl.UncompressedSize,
+                                            .compressedBytes = oldValue.compressedBytes + fl.CompressedSize
+                                        }
+                                    End Function)
+                               End Sub)
+
+            ' Filter and convert to list after aggregation
+            extClassResults = extRes.Values.Where(Function(f) f.cRatio > 0.95).ToList()
+        End Sub)
+
+        Return extClassResults
     End Function
 
     Private Function DetectCompression(fInfo As FileInfo) As CompressionAlgorithm
