785c3b0f5abeea83768647b8b2c4b3063ba8b0d6
IridiumIO <iridium.io@outlook.com>
2024-07-05 22:34:55 +1000

    Refactor settings menu

---
+++ CompactGUI/Views/SettingsView.xaml.vb
@@ -0,0 +1,16 @@
+﻿Public Class SettingsView
+
+    Public Property SettingsVM As New SettingsViewModel
+
+    Sub New()
+
+        Me.DataContext = SettingsVM
+
+        InitializeComponent()
+
+    End Sub
+
+
+
+
+End Class
