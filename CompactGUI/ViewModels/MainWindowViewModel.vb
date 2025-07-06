92c3742f84c96b330a13b48df27dabee417fef03
IridiumIO <iridium.io@outlook.com>
2025-07-06 13:48:05 +1000

    Use source generators for properties and relaycommands in MainWindowViewModel

--- CompactGUI/ViewModels/MainWindowViewModel.vb
+++ CompactGUI/ViewModels/MainWindowViewModel.vb
@@ -1,12 +1,13 @@
 ﻿
-Imports System.ComponentModel
-
 Imports CommunityToolkit.Mvvm.ComponentModel
 Imports CommunityToolkit.Mvvm.Input
 Imports CommunityToolkit.Mvvm.Messaging
 
 
-Public Class MainWindowViewModel : Inherits ObservableObject : Implements IRecipient(Of BackgroundImageChangedMessage)
+Partial Public Class MainWindowViewModel : Inherits ObservableRecipient : Implements IRecipient(Of BackgroundImageChangedMessage)
+
+    <ObservableProperty>
+    Private _BackgroundImage As BitmapImage
 
     Private ReadOnly _watcher As Watcher.Watcher
     Private ReadOnly _windowService As IWindowService
@@ -14,15 +15,25 @@ Public Class MainWindowViewModel : Inherits ObservableObject : Implements IRecip
     Public Sub New(windowService As IWindowService, watcher As Watcher.Watcher)
         _watcher = watcher
         _windowService = windowService
-        WeakReferenceMessenger.Default.Register(Of BackgroundImageChangedMessage)(Me)
+        IsActive = True
+    End Sub
+
+    Public ReadOnly Property IsAdmin As Boolean
+        Get
+            Dim principal = New Security.Principal.WindowsPrincipal(Security.Principal.WindowsIdentity.GetCurrent())
+            Return principal.IsInRole(Security.Principal.WindowsBuiltInRole.Administrator)
+        End Get
+    End Property
+
+
+    <RelayCommand>
+    Private Sub NotifyIconOpen()
+        _windowService.ShowMainWindow()
     End Sub
 
 
-    Public ReadOnly Property NotifyIconOpenCommand As IRelayCommand = New RelayCommand(Sub() _windowService.ShowMainWindow())
-    Public ReadOnly Property NotifyIconExitCommand As IRelayCommand = New RelayCommand(Sub() NotifyExit())
-
-
-    Private Async Function NotifyExit() As Task
+    <RelayCommand>
+    Private Async Function NotifyIconExit() As Task
         If _watcher.WatchedFolders.Count = 0 Then Application.Current.Shutdown()
         Dim confirmed = Await _windowService.ShowMessageBox("CompactGUI", $"You currently have {_watcher.WatchedFolders.Count} folders being watched. Closing CompactGUI will stop them from being monitored.{Environment.NewLine}{Environment.NewLine}Are you sure you want to exit?")
         If Not confirmed Then Return
@@ -30,10 +41,9 @@ Public Class MainWindowViewModel : Inherits ObservableObject : Implements IRecip
         Application.Current.Shutdown()
     End Function
 
-    Public ReadOnly Property ClosingCommand As IRelayCommand = New RelayCommand(Of CancelEventArgs)(AddressOf Closing)
 
-
-    Private Sub Closing(e As CancelEventArgs)
+    <RelayCommand>
+    Private Sub Closing(e As ComponentModel.CancelEventArgs)
         If e Is Nothing Then Return
 
         If Keyboard.Modifiers = ModifierKeys.Shift Then
@@ -54,17 +64,9 @@ Public Class MainWindowViewModel : Inherits ObservableObject : Implements IRecip
     End Sub
 
 
-
-    Public Property BackgroundImage As BitmapImage
-    Public ReadOnly Property IsAdmin As Boolean
-        Get
-            Dim principal = New Security.Principal.WindowsPrincipal(Security.Principal.WindowsIdentity.GetCurrent())
-            Return principal.IsInRole(Security.Principal.WindowsBuiltInRole.Administrator)
-        End Get
-    End Property
-
-
     Public Sub Receive(message As BackgroundImageChangedMessage) Implements IRecipient(Of BackgroundImageChangedMessage).Receive
         BackgroundImage = message.Value
     End Sub
+
+
 End Class
