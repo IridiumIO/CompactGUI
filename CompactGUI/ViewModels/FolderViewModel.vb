83d8c0a9c04c8c1ee6ac3ec3026d101f024f6269
IridiumIO <iridium.io@outlook.com>
2025-07-06 13:35:27 +1000

    Use source generators for properties and relaycommands in FolderViewModel

--- CompactGUI/ViewModels/FolderViewModel.vb
+++ CompactGUI/ViewModels/FolderViewModel.vb
@@ -6,20 +6,37 @@ Imports CommunityToolkit.Mvvm.Input
 
 Imports Wpf.Ui.Controls
 
-<PropertyChanged.AddINotifyPropertyChangedInterface>
+
 Public NotInheritable Class FolderViewModel : Inherits ObservableObject : Implements IDisposable
 
+    <ObservableProperty>
+    Private _Folder As CompressableFolder
 
-    Public Event PropertyChanged As PropertyChangedEventHandler
+    <ObservableProperty>
+    Private _CompressionProgress As Integer
 
-    Public Property Folder As CompressableFolder
+    <ObservableProperty>
+    Private _CompressionProgressFile As String
+
+
+    Private ReadOnly _watcher As Watcher.Watcher
+    Private ReadOnly _snackbarService As CustomSnackBarService
+
+    Public Sub New(folder As CompressableFolder, watcher As Watcher.Watcher, snackbarService As CustomSnackBarService)
+        Me.Folder = folder
+        _watcher = watcher
+        _snackbarService = snackbarService
+        AddHandler folder.PropertyChanged, AddressOf OnFolderPropertyChanged
+        AddHandler folder.CompressionProgressChanged, AddressOf OnFolderCompressionProgressChanged
+        AddHandler folder.CompressionOptions.PropertyChanged, AddressOf OnFolderCompressionOptionsPropertyChanged
+    End Sub
+
 
     Public ReadOnly Property IsAnalysing As Boolean
         Get
             Return Folder?.FolderActionState = ActionState.Analysing
         End Get
     End Property
-
 
     Public ReadOnly Property IsNotResultsOrAnalysing As Boolean
         Get
@@ -37,7 +54,6 @@ Public NotInheritable Class FolderViewModel : Inherits ObservableObject : Implem
         End Get
     End Property
 
-
     Public ReadOnly Property TotalCompressedFiles As Integer
         Get
             Return Folder.AnalysisResults.Where(Function(x) x.CompressionMode <> Core.WOFCompressionAlgorithm.NO_COMPRESSION).Count()
@@ -54,107 +70,6 @@ Public NotInheritable Class FolderViewModel : Inherits ObservableObject : Implem
             .FirstOrDefault()
         End Get
     End Property
-
-
-    Private _watcher As Watcher.Watcher
-    Private ReadOnly _snackbarService As CustomSnackBarService
-
-    Public Sub New(folder As CompressableFolder, watcher As Watcher.Watcher, snackbarService As CustomSnackBarService)
-        Me.Folder = folder
-        _watcher = watcher
-        _snackbarService = snackbarService
-        AddHandler folder.PropertyChanged, AddressOf OnFolderPropertyChanged
-        AddHandler folder.CompressionProgressChanged, AddressOf OnCompressionProgressChanged
-        AddHandler folder.CompressionOptions.PropertyChanged, AddressOf OnCompressionOptionsPropertyChanged
-    End Sub
-
-    Private Sub OnCompressionOptionsPropertyChanged(sender As Object, e As PropertyChangedEventArgs)
-        Dim compressionOptions = CType(sender, CompressionOptions)
-        If e.PropertyName = NameOf(compressionOptions.SelectedCompressionMode) Then
-            OnPropertyChanged(NameOf(DisplayedFolderAfterSize))
-        End If
-    End Sub
-
-    Public Property CompressionProgress As Integer
-    Public Property CompressionProgressFile As String
-
-    Private Sub OnCompressionProgressChanged(sender As Object, e As Core.CompressionProgress)
-        CompressionProgress = e.ProgressPercent
-        CompressionProgressFile = e.FileName.Replace(Folder.FolderName, "")
-    End Sub
-
-    Private Sub OnFolderPropertyChanged(sender As Object, e As PropertyChangedEventArgs)
-        If e.PropertyName = NameOf(Folder.FolderActionState) Then
-            Debug.WriteLine("FolderActionState changed to " & Folder.FolderActionState.ToString())
-            OnPropertyChanged(NameOf(IsAnalysing))
-            OnPropertyChanged(NameOf(IsNotResultsOrAnalysing))
-            OnPropertyChanged(NameOf(CompressionDisplayLevel))
-            OnPropertyChanged(NameOf(DisplayedFolderAfterSize))
-        End If
-    End Sub
-
-
-    Public ReadOnly Property CompressAgainCommand As RelayCommand = New RelayCommand(Sub() Folder.FolderActionState = ActionState.Idle)
-
-    Public ReadOnly Property UncompressCommand As AsyncRelayCommand = New AsyncRelayCommand(AddressOf UncompressFolderAsync)
-
-    Public Async Function UncompressFolderAsync() As Task
-        Await Folder.UncompressFolder()
-        _watcher.UpdateWatched(Folder.FolderName, Folder.Analyser, False)
-
-    End Function
-
-
-    Public ReadOnly Property ApplyToAllCommand As RelayCommand = New RelayCommand(AddressOf ApplyToAll)
-    Private Sub ApplyToAll()
-        Dim allFolders = Application.GetService(Of HomeViewModel)().Folders
-
-        For Each fl In allFolders.Where(Function(f) f.FolderActionState <> ActionState.Analysing AndAlso f.FolderActionState <> ActionState.Working AndAlso f.FolderActionState <> ActionState.Paused)
-            If fl IsNot Folder Then
-                fl.CompressionOptions = Folder.CompressionOptions.Clone
-                fl.FolderActionState = ActionState.Idle
-            End If
-        Next
-
-
-        _snackbarService.ShowAppliedToAllFolders()
-    End Sub
-
-
-    Public ReadOnly Property PauseCommand As IRelayCommand = New RelayCommand(Sub()
-
-                                                                                  If Folder.FolderActionState = ActionState.Working Then
-                                                                                      Folder.Compressor?.Pause()
-                                                                                      Folder.FolderActionState = ActionState.Paused
-                                                                                  Else
-                                                                                      Folder.Compressor?.Resume()
-                                                                                      Folder.FolderActionState = ActionState.Working
-
-                                                                                  End If
-                                                                              End Sub)
-
-    Public ReadOnly Property CancelCommand As IRelayCommand = New RelayCommand(Sub() Folder.Compressor?.Cancel())
-
-    Public ReadOnly Property SubmitToWikiCommand As IRelayCommand = New AsyncRelayCommand(AddressOf SubmitToWiki, AddressOf CanSubmitToWiki)
-
-    Private Async Function SubmitToWiki() As Task
-
-        SubmitToWikiCommand.NotifyCanExecuteChanged()
-
-        Dim result = Await Application.GetService(Of IWikiService).SubmitToWiki(Folder.FolderName, Folder.AnalysisResults.ToList, Folder.PoorlyCompressedFiles, Folder.CompressionOptions.SelectedCompressionMode)
-
-        Folder.IsFreshlyCompressed = False
-        SubmitToWikiCommand.NotifyCanExecuteChanged()
-    End Function
-
-    Private Function CanSubmitToWiki() As Boolean
-        Return TypeOf (Folder) _
-            Is SteamFolder AndAlso
-            Folder.IsFreshlyCompressed AndAlso
-            Not Folder.CompressionOptions.SkipPoorlyCompressedFileTypes AndAlso
-            Not Folder.CompressionOptions.SkipUserSubmittedFiletypes
-    End Function
-
 
     Public ReadOnly Property DisplayedFolderAfterSize As Long
         Get
@@ -179,11 +94,100 @@ Public NotInheritable Class FolderViewModel : Inherits ObservableObject : Implem
     End Property
 
 
+    Private Sub OnFolderCompressionOptionsPropertyChanged(sender As Object, e As PropertyChangedEventArgs)
+        Dim compressionOptions = CType(sender, CompressionOptions)
+        If e.PropertyName = NameOf(compressionOptions.SelectedCompressionMode) Then
+            OnPropertyChanged(NameOf(DisplayedFolderAfterSize))
+        End If
+    End Sub
+
+
+    Private Sub OnFolderCompressionProgressChanged(sender As Object, e As Core.CompressionProgress)
+        CompressionProgress = e.ProgressPercent
+        CompressionProgressFile = e.FileName.Replace(Folder.FolderName, "")
+    End Sub
+
+
+    Private Sub OnFolderPropertyChanged(sender As Object, e As PropertyChangedEventArgs)
+        If e.PropertyName = NameOf(Folder.FolderActionState) Then
+            OnPropertyChanged(NameOf(IsAnalysing))
+            OnPropertyChanged(NameOf(IsNotResultsOrAnalysing))
+            OnPropertyChanged(NameOf(CompressionDisplayLevel))
+            OnPropertyChanged(NameOf(DisplayedFolderAfterSize))
+        End If
+    End Sub
+
+
+    <RelayCommand>
+    Private Sub CompressAgain()
+        Folder.FolderActionState = ActionState.Idle
+    End Sub
+
+    <RelayCommand>
+    Private Async Function Uncompress() As Task
+        Await Folder.UncompressFolder()
+        _watcher.UpdateWatched(Folder.FolderName, Folder.Analyser, False)
+    End Function
+
+    <RelayCommand>
+    Private Sub ApplyToAll()
+        Dim allFolders = Application.GetService(Of HomeViewModel)().Folders
+
+        For Each fl In allFolders.Where(Function(f) f.FolderActionState <> ActionState.Analysing AndAlso f.FolderActionState <> ActionState.Working AndAlso f.FolderActionState <> ActionState.Paused)
+            If fl IsNot Folder Then
+                fl.CompressionOptions = Folder.CompressionOptions.Clone
+                fl.FolderActionState = ActionState.Idle
+            End If
+        Next
+
+        _snackbarService.ShowAppliedToAllFolders()
+    End Sub
+
+    <RelayCommand>
+    Private Sub Pause()
+
+        If Folder.FolderActionState = ActionState.Working Then
+            Folder.Compressor?.Pause()
+            Folder.FolderActionState = ActionState.Paused
+        Else
+            Folder.Compressor?.Resume()
+            Folder.FolderActionState = ActionState.Working
+
+        End If
+    End Sub
+
+    <RelayCommand>
+    Private Sub Cancel()
+        Folder.Compressor?.Cancel()
+    End Sub
+
+    <RelayCommand>
+    Private Async Function SubmitToWiki() As Task
+
+        SubmitToWikiCommand.NotifyCanExecuteChanged()
+
+        Dim result = Await Application.GetService(Of IWikiService).SubmitToWiki(Folder.FolderName, Folder.AnalysisResults.ToList, Folder.PoorlyCompressedFiles, Folder.CompressionOptions.SelectedCompressionMode)
+
+        Folder.IsFreshlyCompressed = False
+        SubmitToWikiCommand.NotifyCanExecuteChanged()
+    End Function
+    Private Function CanSubmitToWiki() As Boolean
+        Return TypeOf (Folder) _
+            Is SteamFolder AndAlso
+            Folder.IsFreshlyCompressed AndAlso
+            Not Folder.CompressionOptions.SkipPoorlyCompressedFileTypes AndAlso
+            Not Folder.CompressionOptions.SkipUserSubmittedFiletypes
+    End Function
+
+
+
+
+
 
     Public Sub Dispose() Implements IDisposable.Dispose
         RemoveHandler Folder.PropertyChanged, AddressOf OnFolderPropertyChanged
-        RemoveHandler Folder.CompressionProgressChanged, AddressOf OnCompressionProgressChanged
-        RemoveHandler Folder.CompressionOptions.PropertyChanged, AddressOf OnCompressionOptionsPropertyChanged
+        RemoveHandler Folder.CompressionProgressChanged, AddressOf OnFolderCompressionProgressChanged
+        RemoveHandler Folder.CompressionOptions.PropertyChanged, AddressOf OnFolderCompressionOptionsPropertyChanged
     End Sub
 
 
