516d8c34793c9ada07084391e286c760d78fa874
IridiumIO <iridium.io@outlook.com>
2026-08-25 23:04:19 +1000

    Delete old VB Core

--- CompactGUI.CoreVB/ICompressor.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Interface ICompressor : Inherits IDisposable
-    Function RunAsync(filesList As List(Of String), Optional progressMonitor As IProgress(Of CompressionProgress) = Nothing, Optional maxParallelism As Integer = 1) As Task(Of Boolean)
-    Sub Pause()
-    Sub [Resume]()
-    Sub Cancel()
-
-End Interface
