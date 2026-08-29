9bd6cf0529396b6dcb858777f7768aa617a53779
IridiumIO <iridium.io@outlook.com>
2026-08-29 19:35:01 +1000

    move update handlignt o lazy translate

--- CompactGUI/Services/LocalisationService.vb
+++ CompactGUI/Services/LocalisationService.vb
@@ -1,22 +1,14 @@
 Imports System.IO
-Imports System.Net.Http
-Imports System.Text
 
 Imports CompactGUI.Core.Settings
 
 Imports LazyTranslate
 
-Imports Tomlyn
-Imports Tomlyn.Model
-
 Public NotInheritable Class LocalisationService
 
     Public Const DefaultCulture As String = "en-AU"
 
-    Private Const LocalisationBaseUrl As String = "https://raw.githubusercontent.com/IridiumIO/CompactGUI/refs/heads/master/CompactGUI/Resources/Localisation/"
-    Private Const ManifestUrl As String = LocalisationBaseUrl & "manifest.toml"
-
-    Private Shared ReadOnly HttpClient As New HttpClient()
+    Private Shared ReadOnly LocalisationBaseUri As New Uri("https://raw.githubusercontent.com/IridiumIO/CompactGUI/refs/heads/master/CompactGUI/Resources/Localisation/")
 
     Private ReadOnly _settingsService As ISettingsService
 
@@ -57,39 +49,7 @@ Public NotInheritable Class LocalisationService
     End Function
 
     Public Async Function CheckForLanguageUpdate() As Task(Of Boolean)
-        Try
-            Dim languageCode = _settingsService.AppSettings.Language
-            Dim localPath = Path.Combine(LocalisationFolder, languageCode & ".toml")
-
-            Dim manifestText = Await HttpClient.GetStringAsync(ManifestUrl)
-            Dim manifest = TomlSerializer.Deserialize(Of TomlTable)(manifestText)
-            Dim locales = TryCast(manifest("locales"), TomlTable)
-
-            If locales Is Nothing OrElse Not locales.ContainsKey(languageCode) Then Return False
-
-            Dim locale = TryCast(locales(languageCode), TomlTable)
-            If locale Is Nothing Then Return False
-
-            Dim remoteVersion = Convert.ToInt32(locale("version"))
-            Dim remoteFileName = CStr(locale("file"))
-            Dim localVersion = If(File.Exists(localPath),
-                                  LazyTranslate.ReadLocalisationVersion(Await File.ReadAllTextAsync(localPath)),
-                                  0)
-
-            If remoteVersion <= localVersion Then Return False
-
-            Dim remoteText = Await HttpClient.GetStringAsync(LocalisationBaseUrl & remoteFileName)
-            If LazyTranslate.ReadLocalisationVersion(remoteText) <> remoteVersion Then Return False
-
-            Dim tempPath = localPath & ".tmp"
-            Await File.WriteAllTextAsync(tempPath, remoteText, Encoding.UTF8)
-            File.Move(tempPath, localPath, True)
-
-            Return True
-        Catch ex As Exception
-            Debug.WriteLine($"[LOC] Failed to check for localisation update: {ex.Message}")
-            Return False
-        End Try
+        Return Await LazyTranslate.CheckForLanguageUpdateAsync(_settingsService.AppSettings.Language, LocalisationBaseUri)
     End Function
 
 End Class
